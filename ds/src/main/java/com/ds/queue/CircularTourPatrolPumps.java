67eef2ce351180001ec9f999a403022ef9e0178c
anand kumar <anandkmr994@gmail.com>
2017-06-22 22:51:04 +0530

    ds learn

--- ds/src/main/java/com/ds/queue/CircularTourPatrolPumps.java
+++ ds/src/main/java/com/ds/queue/CircularTourPatrolPumps.java
@@ -10,5 +10,40 @@ package com.ds.queue;
  * @author anandkumar
  */
 public class CircularTourPatrolPumps {
+	
+	public static void tour(PatrolPump[] arr) {
+		if(null == arr || arr.length == 0){
+			System.out.println("empty array");
+			return ;
+		}
+		
+		int tourStart = 0, tourEnd = 0 ;
+		
+		int distanceCovered = arr[0].patrol-arr[0].distanceToGo;
+		int coveredPoints = 1 ;
+		
+		while(true){
+			while(distanceCovered <0){
+				tourStart++;
+				coveredPoints--;
+			}
+			
+		}
+	}
+    public static void main(String[] args){
+        
+    }
     
+    
+    
+    
+    class PatrolPump{
+		int patrol ;
+		int distanceToGo ;
+		public PatrolPump(int patrol, int distanceToGo) {
+			super();
+			this.patrol = patrol;
+			this.distanceToGo = distanceToGo;
+		}
+	}
 }
